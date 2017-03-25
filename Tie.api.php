b1a5f6245a25b5cbf52fa4c7c8609bbdbcc711b2
arumi-s <andrewwong2008mc@gmail.com>
2017-03-26 00:34:34 +0900

    Update Tie.api.php

--- Tie.api.php
+++ Tie.api.php
@@ -9,15 +9,20 @@ class ApiTie extends ApiBase {
 		if ( $user->isAnon() || $user->isBlocked() ) {
 			$this->getResult()->addValue( null, 'isLogin', 0 );
 		} else {
-			global $wgUploadPath;
+			global $wgUploadPath, $wgLogo;
 			$id = $user->getId();
 			$name = $user->getRealName();
-			if ( $name == '' ) $name =$user->getName();
-			$avatar = new wAvatar( $id, 'l' );
+			if ( $name == '' ) $name = $user->getName();
+			if ( class_exists( 'wAvatar' ) ) {
+				$avatar = new wAvatar( $id, 'l' );
+				$avatar = $wgUploadPath . '/avatars/' . $avatar->getAvatarImage();
+			} else {
+				$avatar = $wgLogo;
+			}
 
 			$this->getResult()->addValue( null, 'userId', (string)$id );
 			$this->getResult()->addValue( null, 'isLogin', 1 );
-			$this->getResult()->addValue( null, 'avatar', $wgUploadPath . '/avatars/' . $avatar->getAvatarImage() );
+			$this->getResult()->addValue( null, 'avatar', $avatar );
 			$this->getResult()->addValue( null, 'nickName', $name );
 		}
 
